edacf6fa8e3fbb506601ce01828388f914647597
Rashi Tiwari <166488281+rashitiwari18@users.noreply.github.com>
2024-07-07 18:44:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let userScore=0;
+let compScore=0;
+
+const userScorePara=document.querySelector(".user-score");
+const compScorePara=document.querySelector(".comp-score");
+
+// display this if choices matched
+function draw(){
+    let msg=document.querySelector(".msgBox");
+    msg.innerText='Draw!  Try Again!';
+    msg.style.backgroundColor="#364652";
+}
 
