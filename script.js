1db569b3602fe9e4d57ac6c555d1a36d5a7b6b1f
Rashi Tiwari <166488281+rashitiwari18@users.noreply.github.com>
2024-07-07 18:44:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,19 @@ function draw(){
     msg.innerText='Draw!  Try Again!';
     msg.style.backgroundColor="#364652";
 }
-
+// to display winner i.e. user or computer will get one point.
+const showWinner=(userWin ,userChoice,compChoice)=>{
+    let msg=document.querySelector(".msgBox");
+    msg.style.backgroundColor="Red";
+   if(userWin){
+    msg.innerText=`You win your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor="green";
+    userScore++;
+    userScorePara.innerText=userScore
+   }else{
+    msg.innerText=`You lose ${compChoice} beats your ${userChoice}`
+    msg.style.backgroundColor="Red";
+    compScore++;
+    compScorePara.innerText=compScore;
+   } 
+}
